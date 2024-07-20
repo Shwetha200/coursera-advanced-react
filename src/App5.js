74e213a7fa259bb77ca2a4be21538289a334b5b8
shwetha <skulal386@gmail.com>
2024-07-20 22:57:23 +0530

    task 5 updated

--- src/App5.js
+++ src/App5.js
@@ -1,19 +1,12 @@
 //TASK 5
 // You've learned how to fetch data in React.
-
 // In this code lab, you'll practice fetching some data from the 
 // randomuser.me
 //  website's API
-
 // This code lab's app, once completed, is supposed to show a single customer's data for the Little Lemon Restaurant.
-
 // The starter code shows only an h1 heading, with the following text: "Data pending..."
-
 // Your task is to complete the data fetching using the fetch() function and to handle the returned Promise object using the then() methods.
-
 // In the return statement of the App component, you also need to add an h2 heading showing the customer's name and the customer's image from the data fetched from the random user API.
-
-
 import React from "react";
 
 function App() {
