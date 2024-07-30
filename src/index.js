02f7cf680e85f43842209d3dfee9ca13c843c5ca
shwetha <skulal386@gmail.com>
2024-07-30 14:59:40 +0530

    Task 7

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import App3 from './App3';
 import App4 from './App4';
 import App5 from './App5';
 import App6 from './App6';
+import App7 from './App7';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App6 />
+    <App7 />
   </React.StrictMode>
 );
 
