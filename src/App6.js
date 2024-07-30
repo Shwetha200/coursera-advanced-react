e7fbee6ae4bd44f0d3271ad43e1a1f860d570e4c
shwetha <skulal386@gmail.com>
2024-07-30 13:39:08 +0530

    Task 6 added

---
+++ src/App6.js
@@ -0,0 +1,47 @@
+//Task 6 using useRef State
+
+import { useState, useEffect, useRef } from "react";
+
+export default function App() {
+  const [day, setDay] = useState("Monday");
+  const prevDay = usePrevious(day);
+
+  const getNextDay = () => {
+    if (day === "Monday") {
+      setDay("Tuesday");
+    } else if (day === "Tuesday") {
+      setDay("Wednesday");
+    } else if (day === "Wednesday") {
+      setDay("Thursday");
+    } else if (day === "Thursday") {
+      setDay("Friday");
+    } else if (day === "Friday") {
+      setDay("Saturday");
+    }else if (day === "Saturday") {
+        setDay("Sunday");
+      }else if (day === "Sunday") {
+        setDay("Monday");
+      }
+  };
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>
+        Today is: {day}
+        <br />
+        {prevDay && <span>Previous work day was: {prevDay}</span>}
+      </h1>
+      <button onClick={getNextDay}>Get next day</button>
+    </div>
+  );
+}
+
+function usePrevious(val) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = val;
+  }, [val]);
+
+  return ref.current;
+}
